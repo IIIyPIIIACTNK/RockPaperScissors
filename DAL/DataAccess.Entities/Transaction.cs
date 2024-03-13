f468b1cff670a08d8437ffb74513938fd816b0a4
shu-m <humoressus@mail.ru>
2024-03-13 14:42:43 +0300

    repository pattern impl

--- DAL/DataAccess.Entities/Transaction.cs
+++ DAL/DataAccess.Entities/Transaction.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace DataAccess.Entities
 {
-    public class Transactions : IEntity<string>
+    public class Transaction : IEntity<string>
     {
         public string Id { get; set; }
         public User Sender { get; set; }
--- DAL/DataAccess.Repositories.Implementations/Class1.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace DataAccess.Repositories.Implementations;
-public class Class1
-{
-
-}
